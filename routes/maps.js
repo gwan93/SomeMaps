e8fbb340a7e5ec4d370e330d3d6c939bff9713d9
vagrant <gio_wan@live.ca>
2021-04-26 23:18:50 +0000

    .

--- routes/maps.js
+++ routes/maps.js
@@ -23,14 +23,14 @@ const {deleteMyFav} = require('../db/queries/fav-maps-queries');
 const {isLoggedIn} = require('../public/scripts/middleware');
 
 router.get("/", (req, res) => {
-  res.send('hello world');
-  /*getAllMapsAnon()
+  // res.send('hello world');
+  getAllMapsAnon()
     .then(data => {
       res.send(data);
     })
     .catch(err => {
       res.send(err)
-    })*/
+    })
 });
 
 router.get('/mymaps', isLoggedIn, (req, res) => {
